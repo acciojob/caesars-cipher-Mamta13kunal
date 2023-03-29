d394f3d8bc60ecc4a811044b6ebe48e13d73915b
Mamta <120008345+Mamta13kunal@users.noreply.github.com>
2023-03-29 20:19:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,50 @@
-// Your Script here.
-
-const lookup = {
-  'A': 'N','B': 'O','C': 'P','D': 'Q',
-  'E': 'R','F': 'S','G': 'T','H': 'U',
-  'I': 'V','J': 'W','K': 'X','L': 'Y',
-  'M': 'Z','N': 'A','O': 'B','P': 'C',
-  'Q': 'D','R': 'E','S': 'F','T': 'G',
-  'U': 'H','V': 'I','W': 'J','X': 'K',
-  'Y': 'L','Z': 'M', '?': '?', ',': ','
+var lookup = {
+  A: "N",
+  B: "O",
+  C: "P",
+  D: "Q",
+  E: "R",
+  F: "S",
+  G: "T",
+  H: "U",
+  I: "V",
+  J: "W",
+  K: "X",
+  L: "Y",
+  M: "Z",
+  N: "A",
+  O: "B",
+  P: "C",
+  Q: "D",
+  R: "E",
+  S: "F",
+  T: "G",
+  U: "H",
+  V: "I",
+  W: "J",
+  X: "K",
+  Y: "L",
+  Z: "M",
+  "?": "?",
+  ",": ",",
 };
 
-function rot13(encodedStr){
+function rot13(encodedStr) {
+  var words = encodedStr.split(" "); // String to Array
+  let decodedArr = []; // Your Result goes here
   // Only change code below this line
-var words= encodedStr.split(" ");
-	   let decodedArr = []; // Your Result goes here
-for(let i=0;i<words.length;i++){
-	const word=words[i];
-	decoded_wprd="";
-	for(let j=0;j<word.length;j++){
-		let char=word.charAt(j);
-		decoded_char=look[char];
-		decoded_word+=decoded_char;
-	}
-	decodedArr.push(decoded_word);
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    let decoded_word = "";
+    for (let j = 0; j < word.length; j++) {
+      var char = word.charAt(j);
+      var decoded_char = lookup[char];
+      decoded_word += decoded_char;
+    }
+    decodedArr.push(decoded_word);
+  }
+  return decodedArr.join(" ");
 }
-  return decodedArr.join(" ");//return decodedArr
-}
-
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
