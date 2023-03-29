83918c7cdc167a33db723a47c8aae1bd15b4abcc
Mamta <120008345+Mamta13kunal@users.noreply.github.com>
2023-03-29 15:37:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,11 +11,22 @@ const lookup = {
 };
 
 function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
   // Only change code below this line
-
-  return ;//return decodedArr
+var words= encodedStr.split(" ");
+	   let decodedArr = []; // Your Result goes here
+for(let i=0;i<words.length;i++){
+	const word=words[i];
+	decoded_wprd="";
+	for(let j=0;j<word.length;j++){
+		let char=word.charAt(j);
+		decoded_char=look[char];
+		decoded_word+=decoded_char;
+	}
+	decodedArr.push(decoded_word);
 }
+  return decodedArr.join(" ");//return decodedArr
+}
+
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
